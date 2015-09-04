2bcff24c90253510830e25e1511726dfef125ae2
camillanore <camilla.nore@gmail.com>
2015-09-04 22:29:51 +0200

    Attempted to expand infladata.py and run it in inflastudy -did not work

--- inflastudy/InflaData.py
+++ inflastudy/InflaData.py
@@ -5,7 +5,18 @@ import pandas as pd
 class InflaData(object):
     def __init__(self, filename=None):
         if filename:
-            self.loadDataFile(filename)
+            self.load_data_file(filename)
 
-    def loadDataFile(self, filename):
+    def load_data_file(self, filename):
+        """ Load raw data from csv file. """
         self.raw_data = pd.DataFrame.from_csv(filename, sep=';')
+        self.find_cpi_data()
+
+def find_cpi_data(self):
+        """ Extract the columns from raw_data that has CPI predictions.
+        """
+        cpi_column_names = []
+        for column_name in self.raw_data.columns:
+            if 'cpi' in column_name:
+                cpi_column_names.append(column_name)
+        self.cpi_predictions = self.raw_data.loc[:, cpi_column_names]
